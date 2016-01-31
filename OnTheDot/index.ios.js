37ad03a0b575ada772af50026667eb15c35111c2
Sasha <email": null>
2016-01-31 11:52:30 -0500

    Moved old files to separate dir

---
+++ OnTheDot/index.ios.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+var REQUEST_URL = 'http://localhost:3000'
+var POST_NEW_GAME = '/games'
+
+class OnTheDot extends Component {
+
+  fetchData() {
+    fetch(REQUEST_URL + POST_NEW_GAME, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({player1: "Sasha"})
+    }).then((response)=> response.json()).then((responseData) => {
+    console.log(responseData);
+    AlertIOS.alert(
+        "POST Response",
+        "Response Body -> " + JSON.stringify(responseData.body)
+    )});
+  }
+
+  render() {
+
+    return (
+      <TouchableHighlight onPress={this.fetchData()}>
+      <View style={styles.container}>
+
+        <Text style={styles.welcome}>
+
+        </Text>
+        <Text style={styles.instructions}>
+
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('OnTheDot', () => OnTheDot);

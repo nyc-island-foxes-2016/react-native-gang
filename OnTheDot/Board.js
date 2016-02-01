f7ec12a1206d4e0a328ff6903110cc2722412a43
Peter Wood <email": null>
2016-02-01 09:43:37 -0500

    Refactor board class into own file

---
+++ OnTheDot/Board.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class Board {
+  grid: Array<Array<number>>;
+
+  constructor() {
+    var size = 2;
+    var grid = Array(size);
+    for (var y = 0; y < size; y++) {
+      var row = Array(size);
+      for (var x = 0; x < size; x++) {
+        row[x] = 0;
+      }
+      grid[y] = row;
+    }
+    this.grid = grid;
+  }
+
+  mark(row: number, col: number): Board {
+    this.grid[row][col] = 1;
+    return this;
+  }
+
+  isClicked(row, col) {
+    return this.grid[row][col] === 1;
+  }
+}
+
+module.exports = Board;

f86f2f6114801fa7dbf09dfeed8ea471cea23e63
Peter Wood <email": null>
2016-02-01 11:51:12 -0500

    Update JoinGame with new board / start game button

--- OnTheDot/ios_components/JoinGame.js
+++ OnTheDot/ios_components/JoinGame.js
@@ -6,29 +6,82 @@ import React, {
   TouchableHighlight,
   View
 } from 'react-native';
+import Board from './Board';
+import Dot from './Dot'
 
 var REQUEST_URL = 'http://localhost:3000'
-// var JOIN_NEW_GAME = '/games/' + GAME_ID + '/accept'
+var POST_NEW_GAME = '/games'
 
 class JoinGame extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {board: new Board(), letterPath: ''};
+  }
 
 
-  // setupPlayerTwo() {
-  //   fetch(REQUEST_URL + ACCEPT_GAME, {
-  //     method: "POST",
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'},
-  //       //PASSING IN
-  //     }
-  //   });
-  // }
+  swap() {
+    this.props.navigator.replace({
+      id: 'MainPage'
+    });
+  }
 
+  setupPlayerTwo() {
+    var update_url = REQUEST_URL + POST_NEW_GAME + '/' + this.props.gameId + '/accept';
+
+    fetch(update_url, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        id: this.props.gameId,
+        player: 'Game-' + Math.round(1e6 * Math.random()),
+        board: this.state.letterPath
+      })
+    })
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.swap();
+    });
+  }
+
+  handleDotClick(row: number, col: number, letter: char) {
+    if(this.state.board.isClicked(row, col)) {
+      return;
+    }
+
+    var letterPath = this.state.letterPath;
+
+    this.setState({
+      board: this.state.board.mark(row, col),
+      letterPath: letterPath + letter
+    });
+  }
 
   render() {
+    var letterSet = ['D', 'C', 'B', 'A'];
+
+    var rows = this.state.board.grid.map((dots, row) =>
+      <View key={row} style={styles.row}>
+        {dots.map((clicked, col) =>
+          <Dot
+            key={col}
+            clicked={clicked}
+            onPress={this.handleDotClick.bind(this, row, col, letterSet.pop())}/>
+        )}
+      </View>
+    );
+
     return (
       <View style={styles.container}>
-        <Text> Game ID: {this.props.gameId} </Text>
+        <View>
+          {rows}
+        </View>
+        <TouchableHighlight onPress={this.setupPlayerTwo.bind(this)}>
+          <Text style={styles.welcome}>
+            Start the Game!
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }

227469d5f811e52af9bb74ee0e830f7071c2a101
Zach <email": null>
2016-02-02 16:40:06 -0500

    Refactor styles constant out of index into own file

---
+++ OnTheDot/ios_components/stylesheet.js
@@ -0,0 +1,49 @@
+'use strict';
+import React, {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    margin: 10,
+    padding: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  dot: {
+    height: 60,
+    width: 60,
+    margin: 60,
+    borderRadius: 30,
+  },
+  clickedDot: {
+    backgroundColor: '#DA8',
+    borderColor: '#642',
+    borderStyle: 'solid',
+    borderWidth: 4
+  },
+  unclickedDot: {
+    backgroundColor: '#ABC',
+    borderColor: '#456',
+    borderStyle: 'solid',
+    borderWidth: 4
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+module.exports = styles;

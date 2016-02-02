d92bb13b58a8cb420b24e103858f7e4a4a539a28
Zach <email": null>
2016-02-02 16:43:10 -0500

    Update dot to import stylesheet

--- OnTheDot/ios_components/Dot.js
+++ OnTheDot/ios_components/Dot.js
@@ -5,6 +5,7 @@ import React, {
   TouchableHighlight,
   View
 } from 'react-native';
+import styles from './stylesheet';
 
 class Dot extends Component {
   dotColor() {
@@ -29,48 +30,5 @@ class Dot extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 40,
-    textAlign: 'center',
-    margin: 10,
-    padding: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  dot: {
-    height: 60,
-    width: 60,
-    margin: 60,
-    borderRadius: 30,
-    borderStyle: 'solid',
-    borderWidth: 4,
-    borderLeftWidth: 0,
-    borderTopWidth: 0
-  },
-  clickedDot: {
-    backgroundColor: '#D43',
-    borderColor: '#E54'
-  },
-  unclickedDot: {
-    backgroundColor: '#BBC',
-    borderColor: '#CCD'
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
 
 module.exports = Dot;

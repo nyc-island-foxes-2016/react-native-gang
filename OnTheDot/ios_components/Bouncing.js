d88ab75ffb12e023c37a2bc953da330ca6259227
Peter Wood <email": null>
2016-02-03 10:22:59 -0500

    Update bouncing to allow click and drag

--- OnTheDot/ios_components/Bouncing.js
+++ OnTheDot/ios_components/Bouncing.js
@@ -1,8 +1,8 @@
 'use strict'
 import React, {
   Animated,
-  Easing,
   Component,
+  PanResponder,
   StyleSheet,
   TouchableHighlight,
   View
@@ -10,47 +10,37 @@ import React, {
 import styles from './stylesheet';
 
 class Bouncing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pan: new Animated.ValueXY()
+    }
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([null, {
+        dx: this.state.pan.x,
+        dy: this.state.pan.y
+      }]),
+      onPanResponderRelease: (e, gesture) => {
+        Animated.spring(
+          this.state.pan,
+          {toValue:{x: 0, y: 0}}
+        ).start();
+      }
+    });
+  }
+
   render() {
     this.anim = this.anim || new Animated.Value(0);
     return (
-      <View>
-        <View
-          onPress={() => {
-            Animated.spring(this.anim, {
-              toValue: 0,
-              velocity: 3,
-              tension: -10,
-              friction: 1
-            }).start();
-          }}>
-        </View>
+      <View style={styles.dragContainer}>
         <Animated.View
-          style={[styles.dot,
-            styles.clickedDot,
-            {
-              transform: [
-                {scale: this.anim.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [1, 4]
-                  })
-                },
-                {translateX: this.anim.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [0, 500]
-                  })
-                },
-                {rotate: this.anim.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: ['0deg', '360deg']
-                  })
-                }
-              ]
-            }
-          ]}>
+          {...this.panResponder.panHandlers}
+          style={[this.state.pan.getLayout(), styles.dot, styles.dragDot]}>
         </Animated.View>
       </View>
     );
   }
-}
+};
 
 module.exports = Bouncing;
